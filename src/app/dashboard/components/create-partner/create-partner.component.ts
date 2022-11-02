d7717d99e376fa5e7f1dc1b6c6342b79f28c3177
Žan Stavbar <46623220+zstavba@users.noreply.github.com>
2022-11-02 11:37:21 +0100

    HTML Update

--- src/app/dashboard/components/create-partner/create-partner.component.ts
+++ src/app/dashboard/components/create-partner/create-partner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-partner',
@@ -8,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class CreatePartnerComponent implements OnInit {
 
   public isEditable: boolean = true;
+
+  partnerBasicInformation = new FormGroup({
+    partner_name:  new FormControl(),
+    parener_address: new FormControl(),
+    partner_type: new FormControl(),
+    partner_zip_code: new FormControl(),
+    partner_location: new FormControl(),
+    partner_country: new FormControl(),
+    partner_language: new FormControl()
+  });
 
   constructor() { }
 
